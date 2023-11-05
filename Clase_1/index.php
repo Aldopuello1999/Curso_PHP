f7d292240cd4accf033b9da035e3c396793e8d17
Aldopuello1999 <aldopuello05@gmail.com>
2023-11-05 14:28:03 -0500

    Primera Clase

---
+++ Clase_1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- clase #1
+    se debe definir de la siguiente forma si queremos añadir codigo php dentro del html, se debe abrir (< ?php) 
+    y se debe cerrar (?>) de lo contrario seria incorrecto. -->
+    <h1> <?php echo "Hola mundo";?></h1>
+</body>
+</html>
